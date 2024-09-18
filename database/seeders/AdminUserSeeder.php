97a5921e19fc30f34059e32dfdaeb663a574515e
Verners Mednis <verners.mednis@inbox.lv>
2024-09-18 16:40:58 +0300

    Update authentication (users now have 1 of 3 roles)
    Every user needs to have one of three roles: Regular user (default), Agent, Administrator. New users can register and they are assigned Regular articles role. There should be one Administration user created with database seeds. After registration or login, users get inside the system which would look like a typical adminpanel to manage data: menus, tables, CRUDs for administrator.

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'), // Use a strong password in production
+            'role' => 'admin', // Set role to admin
+        ]);
+    }
+}
